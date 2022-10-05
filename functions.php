692500fd85d259f629b2ccae440457193535c0e2
solidcreature <38133359+solidcreature@users.noreply.github.com>
2022-10-05 11:49:22 +0300

    Referens Page Template
    + Tweaks

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails' );
 	
 //Load styles & scripts	
-function theme_scripts() {
+function showerwp_load_assets() {
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri() );
 	wp_enqueue_script( 'shower-js', get_template_directory_uri() . '/shower/shower.js' );
 	wp_enqueue_style( 'ribbon-css', get_template_directory_uri() . '/shower/themes/ribbon/styles/styles.css' );
@@ -20,11 +20,11 @@ function theme_scripts() {
 }
 
 
-add_action( 'wp_enqueue_scripts', 'theme_scripts' );	
+add_action( 'wp_enqueue_scripts', 'showerwp_load_assets' );	
 
 //Ограничиваем блоки только допустимым списком
-add_filter( 'allowed_block_types_all', 'allow_my_block_types', 10, 2 );
-function allow_my_block_types( $allowed_blocks, $post ){
+add_filter( 'allowed_block_types_all', 'showerwp_allow_block_types', 10, 2 );
+function showerwp_allow_block_types( $allowed_blocks, $post ){
 
 	return [
 		'core/heading',
@@ -42,10 +42,10 @@ function allow_my_block_types( $allowed_blocks, $post ){
 
 }
 
-function gutenberg_examples_06_register_block() {
+function showerwp_register_block() {
 	$block_path = get_template_directory() . '/slide-block/block.json';
 	register_block_type( $block_path );
 }
-add_action( 'init', 'gutenberg_examples_06_register_block' );
+add_action( 'init', 'showerwp_register_block' );
 
 
